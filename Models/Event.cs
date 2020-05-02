56bb7042ec41bbd4c3cb07c7cd73120db01f7418
Daniel Bennett <danielbradleybennett@gmail.com>
2020-05-02 15:41:00 -0500

    built models

---
+++ Models/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmpowerHealthyStudents.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+
+    }
+}
